76a4b25bfb8bfc89b44929b0a09c1e4a9aa980a7
Daniel Gabl <DManstrator@gmail.com>
2018-04-23 16:38:27 +0200

    Fixed Problem that listening on Port 80 immediately returns

--- Main.go
+++ Main.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 )
 
-func doMagic(writter http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(writter, "Hello World!") // send data to client
+func doMagic(writer http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(writer, "Hello World!") // send data to client
 	return
 }
 
 func main() {
 	http.HandleFunc("/stock", doMagic)  // map function to address
-	err := http.ListenAndServe(":80", nil) // listen to http port
+	err := http.ListenAndServe(":1337", nil) // listen to http port not allowed
 	if err != nil {
 		fmt.Sprintf("An Error occured: %s\n", err) // Housten we have a problem
 	}
