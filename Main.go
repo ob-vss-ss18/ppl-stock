9a887058a2f0b77ff83335a981bde521a2acc25e
konopac <konopac@hm.edu>
2018-04-24 11:13:34 +0200

    Application now searches for the "PORT" system variable, which is set by heroku. if it does not exist, a defailt port will be taken.

--- Main.go
+++ Main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func doMagic(writer http.ResponseWriter, req *http.Request) {
@@ -11,8 +12,17 @@ func doMagic(writer http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	// get port from the environment variable (heroku sets this)
+	var port string = os.Getenv("PORT");
+	// or get an default value (e.g. for developing)
+	if port == "" {
+		port = "1337";
+	}
+	println("using port: " + port);
+
 	http.HandleFunc("/stock", doMagic)  // map function to address
-	err := http.ListenAndServe(":1337", nil) // listen to http port not allowed
+	err := http.ListenAndServe(":" + port, nil) // listen to http port not allowed
+	//err := http.ListenAndServe(":1337", nil) // listen to http port not allowed
 	if err != nil {
 		fmt.Sprintf("An Error occured: %s\n", err) // Housten we have a problem
 	}
