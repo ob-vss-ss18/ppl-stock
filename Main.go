c9b895394dd0ad8e1df174d25bd7f45a5e917f48
Ghouz <sven.schatter@gmail.com>
2018-04-24 17:57:02 +0200

    Graphql (#30)
    * #26 add graphql dependency
    
    * #16 add basic graphql example

--- Main.go
+++ Main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"github.com/graphql-go/graphql"
+	"log"
+	"encoding/json"
 )
 
 func doMagic(writer http.ResponseWriter, req *http.Request) {
@@ -12,6 +15,9 @@ func doMagic(writer http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
+	GraphQLTest();
+
 	// get port from the environment variable (heroku sets this)
 	var port string = os.Getenv("PORT");
 	// or get an default value (e.g. for developing)
@@ -26,4 +32,36 @@ func main() {
 	if err != nil {
 		fmt.Sprintf("An Error occured: %s\n", err) // Housten we have a problem
 	}
+}
+
+func GraphQLTest() {
+	// Schema
+	fields := graphql.Fields{
+		"hello": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				return "world", nil
+			},
+		},
+	}
+	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
+	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schema, err := graphql.NewSchema(schemaConfig)
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
+
+	// Query
+	query := `
+		{
+			hello
+		}
+	`
+	params := graphql.Params{Schema: schema, RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON) // {“data”:{“hello”:”world”}}
 }
