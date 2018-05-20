5d3e8601099544eb6fed2189092dbaee6f03d56a
truenicfel <dassler@hm.edu>
2018-05-21 00:29:52 +0200

    This is pretty basic. It only defines how to resolve the fields which are requested. It does not access a database and it does not define all necessary fields for a ski-set. It instead defines 3 basic fields: id, useCase, brand. The request needs a id as param, it accesses a hardcoded database to get the response data.

--- Main.go
+++ Main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"github.com/graphql-go/graphql"
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
+	"github.com/graphql-go/graphql"
 	"github.com/ob-vss-ss18/ppl-stock/pplStock"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/graphql", func(httpResponseWriter http.ResponseWriter, httpRequest *http.Request) {
+	http.HandleFunc("/ppl-stock", func(httpResponseWriter http.ResponseWriter, httpRequest *http.Request) {
 		// httpRequest.URL.Query().Get("query"):
 		// returns "message" (without the qoutes) for the example below
 		bodyBytes, error := ioutil.ReadAll(httpRequest.Body)
@@ -26,7 +26,7 @@ func main() {
 	})
 
 	fmt.Println("Now server is running on port 8080")
-	fmt.Println("Test with Get      : curl --data 'query{greet(name:\"nico\") message}' http://localhost:8080/graphql")
+	fmt.Println("Curl test: curl --data 'query{ski(id:10){brand id useCase}}' http://localhost:8080/ppl-stock")
 	http.ListenAndServe(":8080", nil)
 
 }
@@ -34,7 +34,7 @@ func main() {
 /*
 	A small function to which will call graphql.Do(), which will handle resolving the
 	request and returns the results.
- */
+*/
 func executeQuery(query string) *graphql.Result {
 	result := graphql.Do(graphql.Params{
 		Schema: pplStock.PPLStockSchema,
--- pplStock/schema.go
+++ pplStock/schema.go
@@ -1,57 +1,127 @@
 package pplStock
 
-import "github.com/graphql-go/graphql"
-
-
-var	PPLStockSchema graphql.Schema
-
+import (
+	"github.com/graphql-go/graphql"
+)
 
 /*
-* Automatically called by go to initialize variables defined here.
-*/
+ * This is the only exported type from this file. It defines the
+ * schema of our api.
+ */
+var PPLStockSchema graphql.Schema
+
+type ski struct {
+	id      int
+	useCase string // langlauf, piste, park
+	brand   string // Fischer, Völkl
+}
+
+var exampleSki ski
+
+/*
+ * Automatically called by go to initialize variables defined here.
+ */
 func init() {
 
+	// example data
+	exampleSki = ski{
+		id:      10,
+		useCase: "Langlauf",
+		brand:   "Fischer",
+	}
+
 	/*
-	* Create a query object type with field message by using GraphQLObjectTypeConfig:
-	* 	- Name: name of the type to be created
-	* 	- Fields: a map of created by using GraphQLFields
-	* 	(these are the fields that can be requested in a query)
-	* We use GraphQLFieldConfig to define/configure the message field:
-	*	- Type: type of Field (graphgl.String)
-	*	- Resolve: function which will return the 'Hello World!' message
- 	*/
-	query := graphql.NewObject(
+	 * Defines how to resolve the type "skiType". Each of the
+	 * resolve functions expect as input a ski struct.
+	 */
+	skiType := graphql.NewObject(graphql.ObjectConfig{
+		Name:        "Ski",
+		Description: "A ski.",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type:        graphql.NewNonNull(graphql.Int),
+				Description: "The id of the ski",
+				Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
+					if ski, ok := parameter.Source.(ski); ok {
+						return ski.id, nil
+					}
+					return nil, nil
+				},
+			},
+			"useCase": &graphql.Field{
+				Type:        graphql.String,
+				Description: "The useCase (Langlauf, Piste, Park) of the ski.",
+				Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
+					if ski, ok := parameter.Source.(ski); ok {
+						return ski.useCase, nil
+					}
+					return nil, nil
+				},
+			},
+			"brand": &graphql.Field{
+				Type:        graphql.String,
+				Description: "The brand of the ski.",
+				Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
+					if ski, ok := parameter.Source.(ski); ok {
+						return ski.brand, nil
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+
+	/*
+	 * The query type which will define the fields which can be requested
+	 * from this api. This is basically the definition of the api.
+	 */
+	queryType := graphql.NewObject(
 		graphql.ObjectConfig{
 			Name: "Query",
 			Fields: graphql.Fields{
-				"message": &graphql.Field{
-					Type: graphql.String,
-					Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-						return "Hello World!", nil
-					},
-				},
-				"greet": &graphql.Field{
-					Type: graphql.String,
+				/*
+				 * If someone requests field "ski" an id must be provided with the
+				 * request. The requested subselections will be resolved using the
+				 * definitions is "skiType" and the input param which is given
+				 * by the resolve function.
+				 *
+				 * This could be a good spot to load the data from the database into
+				 * a local struct. That is why there is a method called "loadSkiFrom
+				 * Database".
+				 */
+				"ski": &graphql.Field{
+					Type: skiType,
 					Args: graphql.FieldConfigArgument{
-						"name": &graphql.ArgumentConfig{
-							Type: graphql.String,
+						"id": &graphql.ArgumentConfig{
+							Description: "Id of the requested ski.",
+							Type: graphql.NewNonNull(graphql.Int),
 						},
 					},
-					Resolve: func(input graphql.ResolveParams) (interface{}, error) {
-						return "Hello " + input.Args["name"].(string), nil
+					Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
+						return loadSkiFromDatabase(parameter.Args["id"].(int)), nil
 					},
 				},
 			},
 		})
 
 	/*
-	* Now create a new schema which has the hello world query created above as
-	* top level query.
-	*/
+	 * Now create a new schema which has the hello world query created above as
+	 * top level query.
+	 */
 	PPLStockSchema, _ = graphql.NewSchema(
 		graphql.SchemaConfig{
-			Query: query,
+			Query: queryType,
 		},
 	)
 
+}
+/*
+ * Dummy method this could load a ski given by id from a database.
+ */
+func loadSkiFromDatabase(id int) ski {
+	result := ski{}
+	if (exampleSki.id == id) {
+		result = exampleSki
+	}
+	return result
 }
