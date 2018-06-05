74c2d05fc40bb378c4d56f44b5dccfa41f5c04c7
Sven Schatter <sven.schatter@gmail.com>
2018-06-05 13:17:50 +0200

    #44 add stick schema

--- schema/stick.go
+++ schema/stick.go
@@ -1,20 +1,20 @@
 package schema
 
 import (
-	"github.com/graphql-go/graphql"
-	"github.com/ob-vss-ss18/ppl-stock/models"
+"github.com/graphql-go/graphql"
+"github.com/ob-vss-ss18/ppl-stock/models"
 )
 
-var skiType = graphql.NewObject(graphql.ObjectConfig{
-	Name:        "Ski",
-	Description: "A ski.",
+var stickType = graphql.NewObject(graphql.ObjectConfig{
+	Name:        "Stick",
+	Description: "A stick.",
 
 	Fields: graphql.Fields{
 		"id": &graphql.Field{
 			Type:        graphql.NewNonNull(graphql.Int),
-			Description: "The id of the ski",
+			Description: "The id of the stick",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Id, nil
 				}
 				return nil, nil
@@ -22,20 +22,10 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 		},
 		"usage": &graphql.Field{
 			Type:        graphql.NewNonNull(graphql.String),
-			Description: "The use case of the ski",
+			Description: "The use case of the stick",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Usage, nil
-				}
-				return nil, nil
-			},
-		},
-		"category": &graphql.Field{
-			Type:        graphql.NewNonNull(graphql.String),
-			Description: "The category of the ski",
-			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
-					return ski.Category, nil
 				}
 				return nil, nil
 			},
@@ -44,7 +34,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The usertype of the ski",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Usertype, nil
 				}
 				return nil, nil
@@ -54,7 +44,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The gender by which the ski is intended to be used.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Gender, nil
 				}
 				return nil, nil
@@ -64,17 +54,17 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The manufactorer of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Manufactorer, nil
 				}
 				return nil, nil
 			},
 		},
-		"model": &graphql.Field{
+		"modell": &graphql.Field{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The model of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Model, nil
 				}
 				return nil, nil
@@ -84,7 +74,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.Int),
 			Description: "The length of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Length, nil
 				}
 				return nil, nil
@@ -94,18 +84,8 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.Int),
 			Description: "The best bodyheight for using this ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Bodyheight, nil
-				}
-				return nil, nil
-			},
-		},
-		"bodyweight": &graphql.Field{
-			Type:        graphql.NewNonNull(graphql.Int),
-			Description: "The best bodyweight for using this ski.",
-			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
-					return ski.Bodyweight, nil
 				}
 				return nil, nil
 			},
@@ -114,7 +94,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The color of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Color, nil
 				}
 				return nil, nil
@@ -124,7 +104,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.Float),
 			Description: "The new price of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.PriceNew, nil
 				}
 				return nil, nil
@@ -134,7 +114,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The condition of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Condition, nil
 				}
 				return nil, nil
@@ -144,7 +124,7 @@ var skiType = graphql.NewObject(graphql.ObjectConfig{
 			Type:        graphql.NewNonNull(graphql.String),
 			Description: "The status/availability of the ski.",
 			Resolve: func(parameter graphql.ResolveParams) (interface{}, error) {
-				if ski, ok := parameter.Source.(models.Ski); ok {
+				if ski, ok := parameter.Source.(models.Stick); ok {
 					return ski.Status, nil
 				}
 				return nil, nil
