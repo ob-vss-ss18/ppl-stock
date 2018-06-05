3be3b9c81f4a3d42a9012d4de13b37861b9447c0
Daniel Gabl <DManstrator@gmail.com>
2018-06-05 11:37:15 +0200

    First implementation of getting data via. an URL Query #41

--- Main.go
+++ Main.go
@@ -5,25 +5,38 @@ import (
 	"fmt"
 	"github.com/graphql-go/graphql"
 	"github.com/ob-vss-ss18/ppl-stock/pplStock"
-	"io/ioutil"
 	"net/http"
+	"io/ioutil"
 )
 
 func main() {
 
 	http.HandleFunc("/graphql", func(httpResponseWriter http.ResponseWriter, httpRequest *http.Request) {
-		// httpRequest.URL.Query().Get("query"):
-		// returns "message" (without the qoutes) for the example below
-		bodyBytes, error := ioutil.ReadAll(httpRequest.Body)
-		if error == nil {
-			bodyString := string(bodyBytes[:len(bodyBytes)])
-			result := executeQuery(bodyString)
-			json.NewEncoder(httpResponseWriter).Encode(result)
-		} else {
-			fmt.Println(error)
+		fmt.Print("Req: "); fmt.Println(httpRequest);
+		fmt.Print("Url: "); fmt.Println(httpRequest.URL);
+		fmt.Print("Qry: "); fmt.Println(httpRequest.URL.Query());
+		fmt.Print("Qdt: "); fmt.Println(httpRequest.URL.Query()["query"]);
+		dataArr, err := httpRequest.URL.Query()["query"];
+		if  (!err || len(dataArr) < 1)  {
+			fmt.Println("Get Data from URL-Query failed, trying to get from body instead...");
+
+			// returns "message" (without the qoutes) for the example below
+			bodyBytes, error := ioutil.ReadAll(httpRequest.Body)
+			if error == nil {
+				bodyString := string(bodyBytes[:len(bodyBytes)])
+				result := executeQuery(bodyString)
+				json.NewEncoder(httpResponseWriter).Encode(result)
+			} else {
+				fmt.Println(error)
+			}
+		}  else  {
+			data := dataArr[0];
+			result := executeQuery(data);
+			json.NewEncoder(httpResponseWriter).Encode(result);
 		}
+
 		httpRequest.Body.Close()
-	})
+	});
 
 	fmt.Println("Now server is running on port 8080")
 	fmt.Println("Curl test: curl --data 'query{Ski(id:10){brand id useCase}}' http://localhost:8080/graphql")
@@ -42,7 +55,7 @@ func executeQuery(query string) *graphql.Result {
 		RequestString: query,
 	})
 	if len(result.Errors) > 0 {
-		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
+		fmt.Printf("wrong result, unexpected errors: %v\n\n", result.Errors)
 	}
 	return result
 }
